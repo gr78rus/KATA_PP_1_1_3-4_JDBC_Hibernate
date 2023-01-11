1e5e77eecd6c819a8344731890bbda448168797b
Roman Grigorev <gr78rus@gmail.com>
2023-01-11 20:08:11 +0500

    Java pre-project. Практическая задача 1.1.4 ver 2.1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,29 +7,34 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
+        UserDao userDao = new UserDaoJDBCImpl();
         return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
     }
 }
